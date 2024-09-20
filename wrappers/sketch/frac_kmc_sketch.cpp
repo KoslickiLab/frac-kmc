35a39844c83d58901ae011132e88fc28ad074b14
Mahmudur <mahmudhera93@gmail.com>
2024-09-20 15:34:13 -0400

    change num threads

--- wrappers/sketch/frac_kmc_sketch.cpp
+++ wrappers/sketch/frac_kmc_sketch.cpp
@@ -29,6 +29,7 @@ int main(int argc, char* argv[]) {
     std::string outfilename;
     bool isFasta = false;
     bool isFastq = false;
+    unsigned int num_threads = 1;
 
     if (argc < 3) {
         std::cerr << "Usage: " << argv[0] << " <infilename> <outfilename> [options]" << std::endl;
@@ -39,7 +40,7 @@ int main(int argc, char* argv[]) {
         std::cerr << "  --fa               Input file is in fasta format" << std::endl;
         std::cerr << "  --fq               Input file is in fastq format" << std::endl;
         std::cerr << "  --a                Write abundances" << std::endl;
-        std::cerr << "  --t <int>          Number of threads for each run of kmc" << std::endl;
+        std::cerr << "  --n <int>          Number of threads (default: 1)" << std::endl;
         return 1;
     }
 
@@ -61,7 +62,7 @@ int main(int argc, char* argv[]) {
             isFastq = true;
         } else if (std::string(argv[i]) == "--a") {
             use_abundance = true;
-        } else if (std::string(argv[i]) == "--t" && i + 1 < argc) {
+        } else if (std::string(argv[i]) == "--n" && i + 1 < argc) {
             num_threads = std::atoi(argv[i + 1]);
         }
     }
